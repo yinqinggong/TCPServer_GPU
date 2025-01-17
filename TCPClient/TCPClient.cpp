a34ab38bbc964f8454f822f11c06c7b90906772a
yinqinggong <yinqinggong@126.com>
2025-01-17 16:06:10 +0800

    提交初始版本

---
+++ TCPClient/TCPClient.cpp
@@ -0,0 +1,47 @@
+﻿// TCPClient.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <asio.hpp>
+#include <iostream>
+#include <string>
+
+void send_command(asio::io_context& io_context, const std::string& command) {
+    asio::ip::tcp::resolver resolver(io_context);
+    asio::ip::tcp::socket socket(io_context);
+
+    // Resolve the server address
+    auto endpoints = resolver.resolve("127.0.0.1", "12345");
+    asio::connect(socket, endpoints);
+
+    // Send the command
+    asio::write(socket, asio::buffer(command + '\n'));
+
+    // Read the response
+    std::string response;
+    asio::read_until(socket, asio::dynamic_buffer(response), '\n');
+    std::cout << "Server response: " << response << std::endl;
+}
+
+int main() {
+    try {
+        asio::io_context io_context;
+        send_command(io_context, "GetParam");
+        send_command(io_context, "GetPhoto");
+        send_command(io_context, "Infer");
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ TCPServer_GPU/TCPServer_GPU.cpp
@@ -0,0 +1,84 @@
+﻿#include <asio.hpp>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <thread>
+
+class TcpServer {
+public:
+    TcpServer(asio::io_context& io_context, short port)
+        : acceptor_(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port)) {
+        start_accept();
+    }
+
+    void start_accept() {
+        auto socket = std::make_shared<asio::ip::tcp::socket>(acceptor_.get_executor());  // Use io_context directly
+        acceptor_.async_accept(*socket, [this, socket](const asio::error_code& error) {
+            if (!error) {
+                std::cout << "Client connected\n";
+                start_read(socket);
+            }
+            else {
+                std::cout << "Error on accept: " << error.message() << std::endl;
+            }
+            start_accept();  // Continue accepting new connections
+            });
+    }
+
+    void start_read(std::shared_ptr<asio::ip::tcp::socket> socket) {
+        asio::async_read_until(*socket, asio::dynamic_buffer(data_), '\n',
+            [this, socket](const asio::error_code& error, std::size_t length) {
+                if (!error) {
+                    std::string command = data_.substr(0, length - 1);//移除末尾的换行符
+                    data_.erase(0, length);
+                    std::cout << "Received command: " << command << std::endl;
+                    // Process the command here
+                    handle_command(socket, command);
+                    start_read(socket);  // Continue reading from the client
+                }
+                else if (error == asio::error::eof) {
+                    std::cout << "Connection closed by client (EOF)" << std::endl;
+                    socket->close();  // 关闭socket
+                    //start_accept();   // 继续接受新的连接
+                }
+                else {
+                    std::cout << "Error on read: " << error.message() << std::endl;
+                    socket->close();
+                }
+            });
+    }
+
+    void handle_command(std::shared_ptr<asio::ip::tcp::socket> socket, const std::string& command) {
+        if (command == "GetParam") {
+            // Respond to GetParam command
+            asio::write(*socket, asio::buffer("GetParam response\n"));
+        }
+        else if (command == "GetPhoto") {
+            // Respond to GetPhoto command
+            asio::write(*socket, asio::buffer("GetPhoto response\n"));
+        }
+        else if (command == "Infer") {
+            // Respond to Infer command
+            asio::write(*socket, asio::buffer("Infer response\n"));
+        }
+        else {
+            asio::write(*socket, asio::buffer("Unknown command\n"));
+        }
+    }
+
+private:
+    asio::ip::tcp::acceptor acceptor_;
+    std::string data_;
+};
+
+int main() {
+    try {
+        asio::io_context io_context;
+        TcpServer server(io_context, 12345);
+        io_context.run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return 0;
+}
